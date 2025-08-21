38a19fddb766701131ec14ddc1cafec79ef3a003
don <129620916+DonLeeHH@users.noreply.github.com>
2025-08-22 01:51:36 +0800

    Implement initial skeletal version, Rename Duke to Sid

---
+++ src/main/java/Sid.java
@@ -0,0 +1,12 @@
+public class Sid {
+    public static void main(String[] args) {
+        String hr = "_".repeat(60);
+
+        System.out.println(hr);
+        System.out.println(" Hello! I'm Sid");
+        System.out.println(" What can I do for you?");
+        System.out.println(hr);
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(hr);
+    }
+}
